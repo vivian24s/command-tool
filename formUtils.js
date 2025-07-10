70dd46290a3dd9843a11ecacdd06bcec6ce62a70
biribirichan <railgun_misaka_mikoto_biribiri@yahoo.co.jp>
2025-07-10 22:18:55 +0900

    修正

--- formUtils.js
+++ formUtils.js
@@ -1,16 +1,99 @@
 // 共通フォームユーティリティ
 // 他のツールやHTMLからも利用できるように関数をまとめています。
 
+function set_attribute_modifiers(form) {
+    let attribute_modifiers = [];
+    for(let i = 0; i < form.amount.length; i++){
+        if(form.amount[i].value !== ""){
+            let str = {};
+            str.type = form.amount[i].title;
+            str.amount = parseFloat(form.amount[i].value);
+            str.operation = "add_value";
+            if(form.slot[i].value !== ""){
+                str.slot = form.slot[i].value;
+            }
+            if(form.id[i].value !== ""){
+                str.id = form.id[i].value;
+            }
+            attribute_modifiers.push(str);
+        }
+    }
+    if(attribute_modifiers.length > 0){
+        return "attribute_modifiers=" + JSON.stringify(attribute_modifiers);
+    } else {
+        return "";
+    }
+}
+
+function set_banner_patterns(form) {}
+function set_base_color(form) {}
+function set_bees(form) {}
+function set_block_entity_data(form) {}
+function set_block_state(form) {}
+function set_blocks_attacks(form) {}
+function set_break_sound(form) {}
+function set_bucket_entity_data(form) {}
+function set_bundle_contents(form) {}
+function set_can_break(form) {}
+function set_can_place_on(form) {}
+function set_charged_projectiles(form) {}
+function set_consumable(form) {}
+function set_container(form) {}
+function set_container_loot(form) {}
+function set_custom_data(form) {}
+function set_custom_model_data(form) {}
 function set_custom_name(form) {
-    //変数定義
     let component_custom_name = "";
-    //コンポーネント組み込み
     if(form.custom_name.value !== ""){
         component_custom_name = "custom_name='" + form.custom_name.value + "'";
     }
     return component_custom_name;
 }
-
+function set_damage(form) {
+    if(form.damage.value !== ""){
+        return "damage=" + form.damage.value;
+    } else {
+        return "";
+    }
+}
+function set_damage_resistant(form) {}
+function set_death_protection(form) {}
+function set_debug_stick_state(form) {}
+function set_dyed_color(form) {
+    if(form.dyed_color_flag.value !== ""){
+        return "dyed_color=" + parseInt(form.dyed_color.value.substr(1, 6), 16);
+    } else {
+        return "";
+    }
+}
+function set_enchantable(form) {}
+function set_enchantment_glint_override(form) {}
+function set_enchantments(form) {
+    let enchantments = {};
+    for(let i = 0; i < form.enchantments_levels.length; i++){
+        if(form.enchantments_levels[i].value !== ""){
+            enchantments[form.enchantments_levels[i].title] = parseInt(form.enchantments_levels[i].value);
+        }
+    }
+    if(Object.keys(enchantments).length > 0){
+        return "enchantments=" + JSON.stringify(enchantments);
+    } else {
+        return "";
+    }
+}
+function set_entity_data(form) {}
+function set_equippable(form) {}
+function set_firework_explosion(form) {}
+function set_fireworks(form) {}
+function set_food(form) {}
+function set_glider(form) {}
+function set_instrument(form) {}
+function set_intangible_projectile(form) {}
+function set_item_model(form) {}
+function set_item_name(form) {}
+function set_jukebox_playable(form) {}
+function set_lock(form) {}
+function set_lodestone_tracker(form) {}
 function set_lore(form) {
     let lore = [];
     if(form.lore[0].value !== ""){
@@ -31,7 +114,14 @@ function set_lore(form) {
         return "";
     }
 }
-
+function set_map_color(form) {}
+function set_map_decorations(form) {}
+function set_map_id(form) {}
+function set_max_damage(form) {}
+function set_max_stack_size(form) {}
+function set_note_block_sound(form) {}
+function set_ominous_bottle_amplifier(form) {}
+function set_pot_decorations(form) {}
 function set_potion_contents(form) {
     let potion_contents = {};
     let custom_effects = [];
@@ -64,55 +154,19 @@ function set_potion_contents(form) {
         return "";
     }
 }
-
-function set_attribute_modifiers(form) {
-    let attribute_modifiers = [];
-    for(let i = 0; i < form.amount.length; i++){
-        if(form.amount[i].value !== ""){
-            let str = {};
-            str.type = form.amount[i].title;
-            str.amount = parseFloat(form.amount[i].value);
-            str.operation = "add_value";
-            if(form.slot[i].value !== ""){
-                str.slot = form.slot[i].value;
-            }
-            if(form.id[i].value !== ""){
-                str.id = form.id[i].value;
-            }
-            attribute_modifiers.push(str);
-        }
-    }
-    if(attribute_modifiers.length > 0){
-        return "attribute_modifiers=" + JSON.stringify(attribute_modifiers);
-    } else {
-        return "";
-    }
-}
-
-function set_damage(form) {
-    if(form.damage.value !== ""){
-        return "damage=" + form.damage.value;
-    } else {
-        return "";
-    }
-}
-
-function set_unbreakable(form) {
-    if(form.unbreakable.value !== ""){
-        return "unbreakable={}";
-    } else {
-        return "";
-    }
-}
-
-function set_dyed_color(form) {
-    if(form.dyed_color_flag.value !== ""){
-        return "dyed_color=" + parseInt(form.dyed_color.value.substr(1, 6), 16);
-    } else {
-        return "";
-    }
-}
-
+function set_potion_duration_scale(form) {}
+function set_profile(form) {}
+function set_provides_banner_patterns(form) {}
+function set_provides_trim_material(form) {}
+function set_rarity(form) {}
+function set_recipes(form) {}
+function set_repair_cost(form) {}
+function set_repairable(form) {}
+function set_stored_enchantments(form) {}
+function set_suspicious_stew_effects(form) {}
+function set_tool(form) {}
+function set_tooltip_display(form) {}
+function set_tooltip_style(form) {}
 function set_trim(form) {
     let trim = {};
     if(form.material.value !== "" && form.pattern.value !== ""){
@@ -123,19 +177,15 @@ function set_trim(form) {
         return "";
     }
 }
-
-function set_enchantments(form) {
-    let enchantments = {};
-    for(let i = 0; i < form.enchantments_levels.length; i++){
-        if(form.enchantments_levels[i].value !== ""){
-            enchantments[form.enchantments_levels[i].title] = parseInt(form.enchantments_levels[i].value);
-        }
-    }
-    if(Object.keys(enchantments).length > 0){
-        return "enchantments=" + JSON.stringify(enchantments);
+function set_unbreakable(form) {
+    if(form.unbreakable.value !== ""){
+        return "unbreakable={}";
     } else {
         return "";
     }
 }
-
-// 必要に応じて他の関数も追加してください。
+function set_use_cooldown(form) {}
+function set_use_remainder(form) {}
+function set_weapon(form) {}
+function set_writable_book_content(form) {}
+function set_written_book_content(form) {}
